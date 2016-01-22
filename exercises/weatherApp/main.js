bab6716ca40f2a4b92fc426e9fd6085798fe0467
Thomas Bealer <tbealer1991@gmail.com>
2016-01-22 16:43:59 -0700

    new projects

--- exercises/weatherApp/main.js
+++ exercises/weatherApp/main.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
   
   "url": "http://jsonip.com/",
   "method": "GET",
-  
+  dataType : "jsonp"
   }
 
 
@@ -12,5 +12,17 @@ $.ajax(settings).done(function (response) {
    $("#IP").append(ip);
   console.log(response);
 });
+var latLong={
+  "url": "https://freegeoip.net/json/",
+  "method": "GET",
+}
 
+$.ajax(latLong).done(function (response) {
+  var lat = response.latitude;
+  var lon = response.longitude;
+  $("#longitude").append(lon);
+  $("#latitude").append(lat);
+ console.log(response);
+
+});
 });

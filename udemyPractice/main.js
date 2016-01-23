6df93562f06eea6b392e5c4012bc13968ec5fa1a
Thomas Bealer <tbealer1991@gmail.com>
2016-01-23 13:09:03 -0700

    more organization

--- udemyPractice/main.js
+++ udemyPractice/main.js
@@ -1,5 +1,16 @@
-document.write("Hello world from Javascript")
-var userName = "Tommy Bealer";
+// document.write("Hello world from Javascript")
+// var userName = "Tommy Bealer";
 
-document.write("<br/>")
-document.write(userName);
+// document.write("<br/>")
+// document.write(userName);
+
+function sayHi() {
+  document.getElementById("fun").innerHTML ="Hi";
+}
+
+function addThings(x,y){
+  var x = prompt("What is your favorite number" + "");
+  var y = prompt("What is your least favorite number" + "");
+  var sum = x+y;
+  return sum;
+}
